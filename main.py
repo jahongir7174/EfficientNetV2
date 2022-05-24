1115ce391225ffec9a91e19d9ce4522c02aebbfa
jahongir7174 <jahongir7174@gmail.com>
2022-05-24 11:53:42 +0900

    fix random_seed

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import argparse
 import copy
 import csv
 import os
-import random
 import warnings
 
 import numpy
 import torch
 import tqdm
+from timm import utils
 from torch.utils import data
 from torchvision import transforms
 
@@ -17,33 +17,22 @@ from utils.dataset import Dataset
 
 warnings.filterwarnings("ignore")
 data_dir = os.path.join('..', 'Dataset', 'IMAGENET')
-
-
-def set_seed():
-    random.seed(0)
-    numpy.random.seed(0)
-    torch.manual_seed(0)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 
 def lr(args):
-    lr_base = 0.256 / 4096
-    if not args.distributed:
-        return args.batch_size * lr_base
-    else:
-        return args.batch_size * lr_base * args.world_size
+    return 0.256 * args.batch_size * args.world_size / 4096
 
 
-def batch(images, target, model, criterion):
-    images = images.cuda()
-    target = target.cuda()
+def batch(samples, targets, model, criterion):
+    samples = samples.cuda()
+    targets = targets.cuda()
 
     with torch.cuda.amp.autocast():
-        output = model(images)
+        outputs = model(samples)
 
-    acc1, acc5 = util.accuracy(output, target, top_k=(1, 5))
-    return criterion(output, target), acc1, acc5
+    acc1, acc5 = util.accuracy(outputs, targets, top_k=(1, 5))
+    return criterion(outputs, targets), acc1, acc5
 
 
 def train(args):
@@ -64,7 +53,6 @@ def train(args):
 
     scheduler = nn.StepLR(optimizer)
     criterion = nn.CrossEntropyLoss().cuda()
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     with open(f'weights/step.csv', 'w') as f:
         best = 0
@@ -95,14 +83,15 @@ def train(args):
                     sampler.set_epoch(epoch)
                 p_bar = loader
                 if args.local_rank == 0:
-                    print(('\n' + '%10s' * 2) % ('epoch', 'loss'))
+                    print(('\n' + '%10s' * 3) % ('epoch', 'memory', 'loss'))
                     p_bar = tqdm.tqdm(loader, total=len(loader))
                 model.train()
-                train_loss = util.AverageMeter()
-                for images, target in p_bar:
-                    loss, _, _ = batch(images, target, model, criterion)
+                m_loss = util.AverageMeter()
+                for samples, targets in p_bar:
+                    loss, _, _ = batch(samples, targets, model, criterion)
 
                     optimizer.zero_grad()
+
                     amp_scale.scale(loss).backward()
                     amp_scale.step(optimizer)
                     amp_scale.update()
@@ -111,14 +100,12 @@ def train(args):
                     torch.cuda.synchronize()
 
                     if args.distributed:
-                        loss = loss.data.clone()
-                        torch.distributed.all_reduce(loss)
-                        loss /= args.world_size
+                        loss = utils.reduce_tensor(loss.data, args.world_size)
 
-                    loss = loss.item()
-                    train_loss.update(loss, images.size(0))
+                    m_loss.update(loss.item(), samples.size(0))
                     if args.local_rank == 0:
-                        desc = ('%10s' + '%10.3g') % ('%g/%g' % (epoch + 1, args.epochs), loss)
+                        gpus = '%.4gG' % (torch.cuda.memory_reserved() / 1E9)
+                        desc = ('%10s' * 2 + '%10.3g') % ('%g/%g' % (epoch + 1, args.epochs), gpus, m_loss.avg)
                         p_bar.set_description(desc)
 
                 scheduler.step(epoch + 1)
@@ -127,8 +114,8 @@ def train(args):
                     writer.writerow({'acc@1': str(f'{acc1:.3f}'),
                                      'acc@5': str(f'{acc5:.3f}'),
                                      'epoch': str(epoch + 1).zfill(3),
-                                     'val_loss': str(f'{val_loss.avg:.3f}'),
-                                     'train_loss': str(f'{train_loss.avg:.3f}')})
+                                     'val_loss': str(f'{val_loss:.3f}'),
+                                     'train_loss': str(f'{m_loss.avg:.3f}')})
                     state = {'model': copy.deepcopy(ema_m.model).half()}
                     torch.save(state, f'weights/last.pt')
                     if acc1 > best:
@@ -152,7 +139,6 @@ def test(model=None):
         model.eval()
 
     criterion = torch.nn.CrossEntropyLoss().cuda()
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     dataset = Dataset(os.path.join(data_dir, 'val'),
                       transforms.Compose([transforms.Resize(384),
@@ -164,24 +150,24 @@ def test(model=None):
     top1 = util.AverageMeter()
     top5 = util.AverageMeter()
 
-    val_loss = util.AverageMeter()
+    m_loss = util.AverageMeter()
 
     with torch.no_grad():
-        for images, target in tqdm.tqdm(loader, ('%10s' * 2) % ('acc@1', 'acc@5')):
-            loss, acc1, acc5 = batch(images, target, model, criterion)
+        for samples, targets in tqdm.tqdm(loader, ('%10s' * 2) % ('acc@1', 'acc@5')):
+            loss, acc1, acc5 = batch(samples, targets, model, criterion)
 
             torch.cuda.synchronize()
 
-            top1.update(acc1.item(), images.size(0))
-            top5.update(acc5.item(), images.size(0))
+            top1.update(acc1.item(), samples.size(0))
+            top5.update(acc5.item(), samples.size(0))
 
-            val_loss.update(loss.item(), images.size(0))
+            m_loss.update(loss.item(), samples.size(0))
         acc1, acc5 = top1.avg, top5.avg
         print('%10.3g' * 2 % (acc1, acc5))
     if model is None:
         torch.cuda.empty_cache()
     else:
-        return val_loss, acc1, acc5
+        return m_loss.avg, acc1, acc5
 
 
 def profile(args):
@@ -196,8 +182,6 @@ def profile(args):
 
 
 def main():
-    set_seed()
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch-size', default=128, type=int)
     parser.add_argument('--input-size', default=300, type=int)
@@ -208,15 +192,22 @@ def main():
     parser.add_argument('--test', action='store_true')
 
     args = parser.parse_args()
+
+    args.world_size = int(os.getenv('WORLD_SIZE', 1))
     args.distributed = int(os.getenv('WORLD_SIZE', 1)) > 1
+
     if args.distributed:
-        torch.cuda.set_device(args.local_rank)
-        args.world_size = int(os.getenv('WORLD_SIZE', 1))
+        torch.cuda.set_device(device=args.local_rank)
         torch.distributed.init_process_group(backend='nccl', init_method='env://')
+
     if args.local_rank == 0:
         if not os.path.exists('weights'):
             os.makedirs('weights')
+
+    utils.random_seed(0, args.local_rank)
+
     profile(args)
+
     if args.train:
         train(args)
     if args.test:
